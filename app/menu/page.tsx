4f79ecb0fbc854a644fee20fbd4df226f420eceb
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-07-07 21:14:55 -0600

    Add mizu logo with link to home

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Tabs } from '@/components/Tabs'
+import Link from 'next/link';
+import Image from 'next/image';
+import mizuLogo from "../../public/images/mizu-logo.png";
 
 const MenuPage = () => {
   return (
-    <section className='bg-black bg-dot-white/[0.2] h-screen w-screen flex justify-center'>
+    <section className='bg-black bg-dot-white/[0.2] w-screen h-full flex justify-center'>
+        <Link href={"/"}>
+        <Image
+            alt="Mizu Izakaya Icon"
+            src={mizuLogo}
+            className="w-28 sm:w-24 lg:w-40 mb-4 absolute top-12 left-16"
+        />
+      </Link>
         <div className='w-full max-w-7xl flex flex-col items-center justify-center'>
-            <h1 className="text-5xl lg:text-6xl sm:text-2xl font-fancy text-gold mb-10">Explore the Menu</h1>
+            <h1 className="text-5xl lg:text-6xl sm:text-2xl font-fancy text-gold my-12">Explore the Menu</h1>
             <Tabs />
         </div>
     </section>
--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -91,7 +91,7 @@ export function Tabs() {
   ];
 
   return (
-    <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-7xl mx-auto w-full  items-start justify-start">
+    <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative flex flex-col max-w-7xl mx-auto w-full items-start justify-start">
       <TabsUI tabs={tabs} />
     </div>
   );
