882ee5e8a64c28b7d8ebaea96cc19fd22c711e7b
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-07-08 23:39:15 -0600

    Link to OpenTable

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { AuroraBackground } from "../../components/ui/aurora-background";
+import Link from "next/link";
 
 const Reservations = () => {
   return (
@@ -16,9 +17,12 @@ const Reservations = () => {
         }}
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center font-fancy">
+        <Link
+          href="https://www.opentable.com/r/mizu-izakaya-and-sushi-denver?corrid=0a449288-8333-41a0-aa6d-ea50f8e1d2f5&avt=eyJ2IjoyLCJtIjowLCJwIjowLCJzIjowLCJuIjowfQ&p=2&sd=2024-07-09T19%3A00%3A00"
+          className="text-3xl md:text-7xl font-bold dark:text-white text-center font-fancy"
+        >
           Reserve a Table with Us
-        </div>
+        </Link>
       </motion.div>
     </AuroraBackground>
   );
