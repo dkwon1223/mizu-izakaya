f97d5fd332098eb6026fc742cd72d3edca005973
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-07-08 22:55:12 -0600

    Add basic structure and styling to reservation page

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,28 @@
 import Hero from "@/components/Hero";
 import Image from "next/image";
-import mizuIcon from "../public/images/mizu-logo.png";
+import mizuLogo from "../public/images/mizu-logo.png";
 import About from "@/components/About";
 import { GallerySlideshow } from "@/components/GallerySlideshow";
-import NavBar from "@/components/NavBar";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="relative bg-black bg-contain bg-no-repeat flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-8xl w-screen px-12 py-12 bg-building-exterior bg-cover bg-no-repeat bg-left-bottom">
-        <Image
-          alt="Mizu Izakaya Icon"
-          src={mizuIcon}
-          className="w-28 sm:w-24 lg:w-40 mb-4"
-        />
+        <Link href={"/"}>
+          <Image
+            alt="Mizu Izakaya Icon"
+            src={mizuLogo}
+            className="w-28 sm:w-24 lg:w-40 mb-4 absolute top-12 left-16 z-10 cursor-pointer"
+          />
+        </Link>
         <Hero />
       </div>
       <div className="max-w-8xl w-screen px-12 py-12 text-white bg-black bg-dot-white/[0.2]">
         <About />
       </div>
       <div className="max-w-8xl w-screen px-12 py-12 text-white bg-main bg-cover bg-no-repeat">
-       <GallerySlideshow />
+        <GallerySlideshow />
       </div>
     </main>
   );
--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Reservations = () => {
   return (
-    <div>Reservations</div>
+    <section className='flex flex-col justify-center items-center bg-dark h-screen w-screen'>
+      <h1 className="text-5xl lg:text-6xl sm:text-2xl font-fancy text-gold mb-10">Reserve a Table with Us</h1>
+    </section>
   )
 }
 
