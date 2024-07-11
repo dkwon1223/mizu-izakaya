664e27488b771ce65f2258a45677d310cf6c6a16
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-07-10 19:17:02 -0600

    Add OpenTable widget to reservation page

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -2,9 +2,25 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { AuroraBackground } from "../../components/ui/aurora-background";
-import Link from "next/link";
+import { useEffect, useRef } from "react";
 
 const Reservations = () => {
+  const dataFetch = useRef(false);
+  useEffect(() => {
+    const openTableWrapper: any = document.getElementById("openTableWrapper");
+
+    const script = document.createElement("script");
+    script.id = "openTableWidget";
+    script.type = "text/javascript";
+    script.async = true;
+    script.src = `https://www.opentable.com/widget/reservation/loader?rid=298771&type=standard&theme=wide&color=1&dark=false&iframe=true&domain=com&lang=en-US&newtab=true&ot_source=Restaurant%20website&cfe=true`;
+    if (dataFetch.current) {
+      return;
+    }
+    dataFetch.current = true;
+    openTableWrapper.appendChild(script);
+  }, []);
+
   return (
     <AuroraBackground>
       <motion.div
@@ -17,12 +33,10 @@ const Reservations = () => {
         }}
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
-        <Link
-          href="https://www.opentable.com/r/mizu-izakaya-and-sushi-denver?corrid=0a449288-8333-41a0-aa6d-ea50f8e1d2f5&avt=eyJ2IjoyLCJtIjowLCJwIjowLCJzIjowLCJuIjowfQ&p=2&sd=2024-07-09T19%3A00%3A00"
-          className="text-3xl md:text-7xl font-bold dark:text-white text-center font-fancy"
-        >
+        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center font-fancy">
           Reserve a Table with Us
-        </Link>
+          <div id="openTableWrapper" className="h-12"></div>
+        </div>
       </motion.div>
     </AuroraBackground>
   );
